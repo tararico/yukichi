97b8f012d44156b71039a4a7bb1b1ff84f3526de
tararico <emipu.uq@gmail.com>
2018-08-01 23:51:13 +0900

    fix method

--- lib/yukichi/separater.rb
+++ lib/yukichi/separater.rb
@@ -2,9 +2,28 @@ module Yukichi
   class Separater
     def initialize(num)
       @num = num
-    end	    
-    def jpy_comma
-      @num.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
+      case num.class
+      when "String"
+        @num = num
+      when "Integer"
+        @num = num.to_arabic_numeral
+      end
+    end
+    	    
+    def to_k(money_mark = false) 
+      if money_mark # moneyマークがtrueなら"円"をつける
+        @num.to_chinese_numeral + "円"
+      else
+        @num.to_chinese_numeral
+      end
+    end
+
+    def jpy_comma(money_mark == false)
+      if money_mark == true
+        "¥" + @num.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
+      else
+        @num.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
+      end
     end 
   end
 end

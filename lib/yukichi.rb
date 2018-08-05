66b228b7dc9e82bd351a371c3bbcba3bd0eef6fb
tararico <emipu.uq@gmail.com>
2018-08-05 15:27:17 +0900

    fix initialize method

--- lib/yukichi.rb
+++ lib/yukichi.rb
@@ -6,12 +6,11 @@ class Yukichi
   include Yukichi::Separater
   include Yukichi::Kanji
   def initialize(num)
-    case num.class
+    case num
     when Integer
       @num = num
     when String
       @num = num.to_arabic_numeral
-      raise @num
     end
   end
 end
--- lib/yukichi/version.rb
+++ lib/yukichi/version.rb
@@ -1,3 +1,3 @@
 class Yukichi
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
